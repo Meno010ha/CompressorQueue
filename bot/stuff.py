c47dd93991c3f8f0ef244eb6415c92f975b9ccdb
Meno010ha <91308045+Meno010ha@users.noreply.github.com>
2021-09-24 12:04:07 +0530

    ___main__stuffy.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -59,8 +59,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MAIN CHANNEL", url="@AnimeLowMb"),
+                Button.url("DEVELOPER", url="t.me/akgkmightbot"),
             ],
         ],
     )
